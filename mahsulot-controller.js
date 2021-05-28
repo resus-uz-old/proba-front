e3a85ed76896a8019741afbc416738d841a4e550
Muminniyoz Shonazarov <muminniyoz2912@gmail.com>
2021-05-28 10:20:38 +0500

    Pagination raqamlari sozlanid va size, sort qoshildi

--- mahsulot-controller.js
+++ mahsulot-controller.js
@@ -8,13 +8,15 @@ let currentPage = 0;
 let currentSize = 10;
 let totalElements = 0;
 let totalPages = 0;
+let sort = "id";
+let sortDirection = 'ASC';
 
 
 
 
 function loadData() {
 
-    http.get(`/api/mahsulot?page=${currentPage}&${currentSize}`, function (res) {
+    http.get(`/api/mahsulot?page=${currentPage}&size=${currentSize}&sort=${sort},${sortDirection}`, function (res) {
         let page = JSON.parse(res);
         tbody.innerHTML = "";
         let txt = "";
@@ -47,20 +49,38 @@ function loadData() {
 function pageSetting(page) {
     totalElements = page.totalElements;
     totalPages = page.totalPages;
-    mahsulotSoni.innerHTML = totalPages;
+    mahsulotSoni.innerHTML = totalElements;
     sahifaSoni.innerHTML = page.totalPages;
     joriySahifa.innerHTML = page.number + 1;
     let list = document.querySelector("#pagination-list");
 
-    for (let i = 1; i < list.length-1; i++) {
-        list.children.remove(list.children[i]);
+    // List elementlarini tozalash kerak
+    var lis = document.querySelectorAll('#pagination-list li');
+    console.log(lis)
+    for (var i = 1; i < lis.length - 1; i++) {
+        let li = lis[i]
+        li.parentNode.removeChild(li);
     }
+ let k = 3;
+    let start = currentPage - k;
+    if (start < 1) {
+        start = 1;
+        k += k - currentPage;
+    }
+    let end = currentPage + k;
+    if (end > totalPages) end = totalPages;
 
-    for (let i = 1; i <= totalPages; i++) {
+
+
+
+    for (let i = start; i <= end; i++) {
         let li = document.createElement('li');
         li.classList.add('page-item');
-        li.innerHTML = `<a class="page-link" onclick='pageChange(${i})' >${i}</a>`;
-     
+        if (i == currentPage + 1)
+            li.classList.add('active');
+
+        li.innerHTML = `<a class="page-link"  onclick='pageChange(${i})' >${i}</a>`;
+
 
         list.insertBefore(li, nextBtn);
     }
@@ -68,6 +88,14 @@ function pageSetting(page) {
 
 }
 
+function sizeChange(size) {
+    currentSize = size;
+    console.log(currentSize);
+    if (currentSize * (currentPage + 1) > totalElements) {
+        pageChange(currentPage);
+    } else
+        loadData();
+}
 
 
 function pageChange(target) {
@@ -77,7 +105,7 @@ function pageChange(target) {
     } else if (target === 'back') {
         currentPage--;
     } else {
-        if(typeof(target)=='number' && 1<= target && target <= totalPages){
+        if (typeof (target) == 'number' && 1 <= target && target <= totalPages) {
             currentPage = target - 1;
         }
     }
@@ -98,3 +126,31 @@ function pageChange(target) {
 }
 
 
+let currentSortElement = document.querySelector('th');
+
+function sorting(element, target){
+    if (currentSortElement == element){
+        sortDirection = sortDirection == 'ASC'? 'DESC' : 'ASC';
+    } else {
+        sortDirection = 'ASC';
+    }
+    currentSortElement.style.borderBottom = '';
+
+    let i = currentSortElement.querySelector('i');
+    i.classList.remove('fa-sort-desc');
+          i.classList.remove('fa-sort-asc');
+    currentSortElement = element;
+    currentSortElement.style.borderBottom = 'solid 1px white';
+    if(sortDirection == 'ASC'){
+        let i = currentSortElement.querySelector('i');
+  i.classList.remove('fa-sort-desc');
+        i.classList.add('fa-sort-asc');
+      
+    } else {
+        i.classList.remove('fa-sort-asc');
+        i.classList.add('fa-sort-desc');
+    }
+
+    sort = target;
+    loadData();
+}
